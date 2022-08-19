b15592974552b22b957e4fad96faec104718859c
Eric Qvarnström <info@eqwebsolutions.se>
2022-08-19 08:33:30 +0200

    Added faulty image link

--- src/components/Portfolio/PortfolioLanguage.jsx
+++ src/components/Portfolio/PortfolioLanguage.jsx
@@ -4,7 +4,7 @@ const PortfolioLanguage = ({language})=> {
     return (
         <div className="inline-block px-1">
             <div className="inline-block items-center mt-2 bg-slate-200 w-auto shadow-md hover:shadow-lg hover:bg-slate-300 transition-all">
-                <Image src={"logos/" + language + ".png"} className="h-6 w-6 inline" alt="Logo of language or framework"/>
+                <Image src={"/logos/" + language + ".png"} className="h-6 w-6 inline" alt="Logo of language or framework"/>
                 <p className="inline pr-3 pl-2">{language.charAt(0).toUpperCase() + language.slice(1)}</p>
             </div>
         </div>
