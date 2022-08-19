010a40a8bd309d5ff2da42747dd18c1dd5cd777d
Eric Qvarnström <info@eqwebsolutions.se>
2022-08-19 21:12:44 +0200

    fix: Size of popup was to small on small screens

--- src/components/Portfolio/PortfolioItem.jsx
+++ src/components/Portfolio/PortfolioItem.jsx
@@ -3,14 +3,14 @@ import ImageCarousel from '../../../src/components/Slider/Index'
 
 const PortfolioItem = (props) => {
     return (
-        <div className="w-full md:w-4/5 md:mx-auto flex flex-col md:flex-row justify-around md:text-left text-center mt-10 shadow-xl rounded-xl p-5 items-center">
+        <div className="w-full md:w-4/5 md:mx-auto flex flex-col md:flex-row justify-around md:text-left text-center mt-10 shadow-2xl rounded-xl p-5 items-center">
             <div className="w-full md:w-1/2 order-2 md:order-1">
                 <div className="p-3">
                     <p className="text-2xl text-gray-500 font-bold hidden md:block">{props.title}</p>
                     <p className="text-gray-500 font-bold">{props.roles}</p>
                     <p className="text-md text-gray-500 mt-2 pr-3">{props.content}</p>
 
-                    <div className="ml-0 mt-3">
+                    <div className=" mt-3">
                         {props.languages.map(lang => {
                             return (<PortfolioLanguage key={Math.random()} language={lang}/>)
                         })}
--- src/components/popUpContact/Index.jsx
+++ src/components/popUpContact/Index.jsx
@@ -12,13 +12,13 @@ const Popup = (props) => {
             initial= {{opacity:0}}
             animate={{opacity:1}}
             transition={{duration:.2, delay: .3}}
-            className=" w-11/12 lg:w-4/5 xl:w-1/3 h-1/3 bg-white rounded-xl shadow-xl flex flex-col px-10 text-center">
+            className=" w-11/12 lg:w-4/5 xl:w-1/3 h-auto pb-5 bg-white rounded-xl shadow-xl flex flex-col px-10 text-center">
                 <p className="text-xl font-bold text-slate-600 mt-6">I&apos;m glad to hear that you want to talk to me!</p>
                 <PopUpLink link="mailto:ericqvarnstrom@gmail.com">Ericqvarnstrom@gmail.com</PopUpLink>
                 <PopUpLink link="tel:ericqvarnstrom@gmail.com">+46702258844</PopUpLink>
                 <PopUpLink link="https://www.linkedin.com/in/ericqvarnström/" target="_blank">Linkedin</PopUpLink>
 
-                <button className="mt-10" onClick={props.close}>Close</button>
+                <button className="mt-10 rounded-xl shadow-md bg-slate-300 text-slate-100 hover:text-white hover:bg-slate-400 transition-all gover:shadow-xl py-2"  onClick={props.close}>Close</button>
             </motion.div>
         </motion.div>
     );
