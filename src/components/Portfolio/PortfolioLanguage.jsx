091667f3bf35284111dec867703738b5849b484d
Eric Qvarnström <ericqvarnstrom@MacBook-Pro-som-tillhor-Eric.local>
2023-07-22 21:41:01 +0200

    to lowercase

--- src/components/Portfolio/PortfolioLanguage.jsx
+++ src/components/Portfolio/PortfolioLanguage.jsx
@@ -5,6 +5,9 @@ const PortfolioLanguage = ({language, imgext})=> {
     let lang = language.charAt(0).toUpperCase() + language.slice(1);
     lang = lang.replace('_', ' ');
 
+    let imagename = "/logos/" + language + "." + imgext;
+    imagename = imagename.toLowerCase();
+    
     return (
         <div className="inline-block px-1">
             <div className="inline-block items-center mt-2 bg-slate-200 w-auto shadow-md hover:shadow-lg hover:bg-slate-300 transition-all">
