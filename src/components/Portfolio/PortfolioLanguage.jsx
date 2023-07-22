9a07a8b9de1fc83fbbfdf52ad63b39477eee8182
Eric Qvarnström <ericqvarnstrom@MacBook-Pro-som-tillhor-Eric.local>
2023-07-22 22:02:29 +0200

    Fixed name

--- src/components/Portfolio/PortfolioLanguage.jsx
+++ src/components/Portfolio/PortfolioLanguage.jsx
@@ -12,7 +12,7 @@ const PortfolioLanguage = ({language, imgext})=> {
         <div className="inline-block px-1">
             <div className="inline-block items-center mt-2 bg-slate-200 w-auto shadow-md hover:shadow-lg hover:bg-slate-300 transition-all">
                 <img height="24px" width="24px" src={imagename} className="h-6 w-6 inline" alt="Logo of language or framework"/>
-                <p className="inline pr-3 pl-2">{language.charAt(0).toUpperCase() + language.slice(1)}</p>
+                <p className="inline pr-3 pl-2">{lang}</p>
             </div>
         </div>
         
